46f1411be6947e77846451cf171e777770fcdb0a
KariBB <kari_bautistabarba@hotmail.com>
2025-04-08 21:00:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,18 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/KariBB/MachineLearning/refs/heads/master/final_cleaned_dataset.csv')
   df
-
-st.write('**X**')
-X = df.drop(columns=['subject_id', 'stay_id','weaning_success','age_group'], axis=1)
-X
-
-st.write('**y**')
-y = df.weaning_success
-y
+  
+  st.write('**X**')
+  X = df.drop(columns=['subject_id', 'stay_id','weaning_success','age_group'], axis=1)
+  X
+  
+  st.write('**y**')
+  y = df.weaning_success
+  y
 
 with st.expander('Data Visualization'):
     st.scatter_chart(data=df, x='age', y='bmi', color='weaning_success')
+
+# Data preparation
+with st.sidebar: 
+  st.header('Input features')
