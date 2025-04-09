3e29775f74b9ef0f5f8f3e24040a8f6f93a9550d
KariBB <kari_bautistabarba@hotmail.com>
2025-04-09 11:37:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ with st.sidebar:
   st.header('Input features')
   # charlson_comorbidity_index,chronic_pulmonary_disease,congestive_heart_failure,dementia,severe_liver_disease,renal_disease,rheumatic_disease,diabetes,gcs_total,max_wbc,max_hemoglobin,max_platelets,max_creatinine,max_anion_gap,min_wbc,min_hemoglobin,min_platelets,min_creatinine,min_anion_gap,max_hr,max_map,max_resp_rate,max_spo2,max_temp,min_hr,min_map,min_resp_rate,min_spo2,min_temp,duration_imv_hours,duration_niv_hours,duration_other_niv_houress,age_group
   age = st.slider('age (years)',18,99)
+  gcs_total = st.slider('GCS Total',0,20)
   gender = st.selectbox('gender',('M','F'))
   bmi = st.number_input('BMI', min_value=10.0, max_value=80.0, value=25.0, step=0.1)
   charlson_comorbidity_index = st.number_input('Charlson Combordity Index', min_value=0, max_value=20, value=10, step=1)
