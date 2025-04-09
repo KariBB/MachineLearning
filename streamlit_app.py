d350f8e4e22fe3b393d729c39096909de140c126
KariBB <kari_bautistabarba@hotmail.com>
2025-04-08 20:41:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🤖 Machine Learning App')
 
 st.write('This is a Machine Learning App for predicting weaning success in patients with sepsis')
 
-df = pd.read_csv('https://raw.githubusercontent.com/KariBB/MachineLearning/refs/heads/master/final_cleaned_dataset.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/KariBB/MachineLearning/refs/heads/master/final_cleaned_dataset.csv')
+  df
